7124f738ac5c3ea8ff81cd7d2ba9b6bb8cf2a6c7
Germán Estévez <NeftaliDelphi@users.noreply.github.com>
2023-02-22 15:34:31 +0100

    Update Inno.bat
    Cambio de path

--- Inno.bat
+++ Inno.bat
@@ -1,2 +1,2 @@
 echo Generar archivo de instalación
-"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" "c:\ProyectosDelphi\LoginApplication\Inno\LoginApp.iss"
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" "c:\ProyectosDelphi\LoginApplication1\Inno\LoginApp.iss"
